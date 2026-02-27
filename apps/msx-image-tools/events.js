5d4703ab121564335fc5d9ea439a30df8ccc71ac
Andre Baptista <albs_br@yahoo.com.br>
2026-02-27 20:35:56 -0300

    v.1.15.6

---
+++ apps/msx-image-tools/events.js
@@ -0,0 +1,152 @@
+const fileInputImage = document.getElementById('fileInputImage');
+const c = document.getElementById("canvasScreen");
+const ctxImage = c.getContext("2d");
+const c1 = document.getElementById("canvasPalette");
+const ctxPalette = c1.getContext("2d");
+const fileInputPalette = document.getElementById("fileInputPalette");
+const rdoPaletteSource_Image = document.getElementById("rdoPaletteSource_Image");
+const rdoPaletteSource_Palette = document.getElementById("rdoPaletteSource_Palette");
+const btnExecuteCommand = document.getElementById("btnExecuteCommand");
+const comboColorFrom = document.getElementById("comboColorFrom");
+const comboColorTo = document.getElementById("comboColorTo");
+const btnSaveFile = document.getElementById("btnSaveFile");
+const div_Cmd_ReplaceColor = document.getElementById("div_Cmd_ReplaceColor");
+const div_Cmd_ConvertToSprites = document.getElementById("div_Cmd_ConvertToSprites");
+
+const btnReset = document.getElementById("btnReset");
+
+const output = document.getElementById('output');
+
+rdoPaletteSource_Palette.addEventListener('click', (event) => {
+    fileInputPalette.hidden = false;
+    processImageRawData();
+});
+
+rdoPaletteSource_Image.addEventListener('click', (event) => {
+    fileInputPalette.hidden = true;
+    processImageRawData();
+});
+
+fileInputImage.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (file) {
+
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            const arrayBuffer = e.target.result;
+            // arrayBuffer now contains the binary data of the file
+            // You can process the binary data here
+            
+            //processBinaryData(arrayBuffer);
+            rawData = [];
+            const dataView = new DataView(arrayBuffer);
+            for(let i = 0; i < dataView.byteLength; i++) {
+                const byteRead = dataView.getUint8(i);
+                rawData.push(byteRead);
+            }
+
+            processImageRawData();
+        };
+
+        reader.onerror = (e) => {
+            console.error("Error reading file:", e);
+        };
+
+        reader.readAsArrayBuffer(file);
+    }
+});
+
+btnExecuteCommand.addEventListener('click', (event) => {
+    try {
+        const colorFrom = comboColorFrom.value;
+        const colorTo = comboColorTo.value;
+        
+        replaceColor(colorFrom, colorTo);
+    }
+    catch {
+        alert('Error');
+    }
+});
+
+fileInputPalette.addEventListener('change', (event) => {
+    const file = event.target.files[0];
+    if (file) {
+
+        palette = [];
+        byteArrayPalette = [];
+        
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            const arrayBuffer = e.target.result;
+            // arrayBuffer now contains the binary data of the file
+            // You can process the binary data here
+
+            const dataView = new DataView(arrayBuffer);
+
+            // if file starts with 0xFE, ignore first 7 bytes (file header)
+            let start;
+            if (dataView.getUint8(0) == 0xFE) {
+                start = 7;
+            }
+            else {
+                start = 0;
+            }
+            
+            //console.log("palette file size: " + dataView.byteLength);
+            if(dataView.byteLength != start + 32) {
+                throw new Error("Palette file must be 32 bytes long (or 39 with header).");
+            }
+
+
+            for(let i = start; i < start + 32; i++) {
+                byteArrayPalette.push(dataView.getUint8(i));
+            }
+            
+            loadPalette(byteArrayPalette, palette);
+
+            processImageRawData();
+        };
+
+        reader.onerror = (e) => {
+            console.error("Error reading file:", e);
+        };
+
+        reader.readAsArrayBuffer(file);
+    }
+});
+
+btnSaveFile.addEventListener('click', (event) => {
+
+    try {
+        const binaryArray = [];
+        for(let i=0; i < pixels.length; i += 2) {
+            const leftPixel = pixels[i] << 4;
+            const rightPixel = pixels[i+1];
+
+            binaryArray.push(leftPixel | rightPixel);
+        }
+
+        const binaryData = new Uint8Array(binaryArray);
+
+        const blob = new Blob([binaryData], { type: 'application/octet-stream' });
+        saveFile(blob, 'image.sc5');    
+    }
+    catch {
+        alert('Error');
+    }
+});
+
+btnReset.addEventListener('click', reset);
+
+comboCommand.addEventListener('change', (event) => {
+    if(comboCommand.value == 0) {
+        div_Cmd_ReplaceColor.hidden = false;
+        div_Cmd_ConvertToSprites.hidden = true;
+    }
+    else if(comboCommand.value == 1) {
+        div_Cmd_ReplaceColor.hidden = true;
+        div_Cmd_ConvertToSprites.hidden = false;
+    }
+});
--- apps/msx-image-tools/script.js
+++ apps/msx-image-tools/script.js
@@ -1,129 +1,3 @@
-const fileInputImage = document.getElementById('fileInputImage');
-const c = document.getElementById("canvasScreen");
-const ctxImage = c.getContext("2d");
-const c1 = document.getElementById("canvasPalette");
-const ctxPalette = c1.getContext("2d");
-const fileInputPalette = document.getElementById("fileInputPalette");
-const rdoPaletteSource_Image = document.getElementById("rdoPaletteSource_Image");
-const rdoPaletteSource_Palette = document.getElementById("rdoPaletteSource_Palette");
-const btnExecuteCommand = document.getElementById("btnExecuteCommand");
-const comboColorFrom = document.getElementById("comboColorFrom");
-const comboColorTo = document.getElementById("comboColorTo");
-const btnSaveFile = document.getElementById("btnSaveFile");
-
-const output = document.getElementById('output');
-
-rdoPaletteSource_Palette.addEventListener('click', (event) => {
-    fileInputPalette.hidden = false;
-    processImageRawData();
-});
-
-rdoPaletteSource_Image.addEventListener('click', (event) => {
-    fileInputPalette.hidden = true;
-    processImageRawData();
-});
-
-fileInputImage.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    if (file) {
-
-        const reader = new FileReader();
-
-        reader.onload = (e) => {
-            const arrayBuffer = e.target.result;
-            // arrayBuffer now contains the binary data of the file
-            // You can process the binary data here
-            
-            //processBinaryData(arrayBuffer);
-            rawData = [];
-            const dataView = new DataView(arrayBuffer);
-            for(let i = 0; i < dataView.byteLength; i++) {
-                const byteRead = dataView.getUint8(i);
-                rawData.push(byteRead);
-            }
-
-            processImageRawData();
-        };
-
-        reader.onerror = (e) => {
-            console.error("Error reading file:", e);
-        };
-
-        reader.readAsArrayBuffer(file);
-    }
-});
-
-btnExecuteCommand.addEventListener('click', (event) => {
-    const colorFrom = comboColorFrom.value;
-    const colorTo = comboColorTo.value;
-    
-    replaceColor(colorFrom, colorTo);
-});
-
-fileInputPalette.addEventListener('change', (event) => {
-    const file = event.target.files[0];
-    if (file) {
-
-        palette = [];
-        byteArrayPalette = [];
-        
-        const reader = new FileReader();
-
-        reader.onload = (e) => {
-            const arrayBuffer = e.target.result;
-            // arrayBuffer now contains the binary data of the file
-            // You can process the binary data here
-
-            const dataView = new DataView(arrayBuffer);
-
-            // if file starts with 0xFE, ignore first 7 bytes (file header)
-            let start;
-            if (dataView.getUint8(0) == 0xFE) {
-                start = 7;
-            }
-            else {
-                start = 0;
-            }
-            
-            //console.log("palette file size: " + dataView.byteLength);
-            if(dataView.byteLength != start + 32) {
-                throw new Error("Palette file must be 32 bytes long (or 39 with header).");
-            }
-
-
-            for(let i = start; i < start + 32; i++) {
-                byteArrayPalette.push(dataView.getUint8(i));
-            }
-            
-            loadPalette(byteArrayPalette, palette);
-
-            processImageRawData();
-        };
-
-        reader.onerror = (e) => {
-            console.error("Error reading file:", e);
-        };
-
-        reader.readAsArrayBuffer(file);
-    }
-});
-
-btnSaveFile.addEventListener('click', (event) => {
-
-    const binaryArray = [];
-    for(let i=0; i < pixels.length; i += 2) {
-        const leftPixel = pixels[i] << 4;
-        const rightPixel = pixels[i+1];
-
-        binaryArray.push(leftPixel | rightPixel);
-    }
-
-    const binaryData = new Uint8Array(binaryArray);
-
-    const blob = new Blob([binaryData], { type: 'application/octet-stream' });
-    saveFile(blob, 'image.sc5');    
-});
-
 let rawData;
 let palette;
 let paletteOnImage;
@@ -210,15 +84,17 @@ const processImageRawData = () => {
 
 const replaceColor = (colorFrom, colorTo) => {
 
-    for(let i = 0; i < pixels.length; i++) {
-        if(pixels[i] == colorFrom) { 
-            pixels[i] = colorTo;
+    if(pixels) {
+        for(let i = 0; i < pixels.length; i++) {
+            if(pixels[i] == colorFrom) { 
+                pixels[i] = colorTo;
+            }
         }
+
+        drawImage();
+
+        updateOutput();
     }
-
-    drawImage();
-
-    updateOutput();
 };
 
 const drawImage = () => {
@@ -326,3 +202,14 @@ const saveFile = async (blob, suggestedName) => {
   link.click();
   URL.revokeObjectURL(link.href);
 };
+
+const reset = () => {
+
+    div_Cmd_ReplaceColor.hidden = false;
+    div_Cmd_ConvertToSprites.hidden = true;
+
+    // TODO: clear both canvas
+};
+
+// initialization
+reset();
